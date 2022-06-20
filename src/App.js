74e4dbb73636f22104aeed7f985b79396bbd4732
MauricioSG <udcarkangel@gmail.com>
2022-06-19 22:58:17 -0500

    First commit, creation y inst-styled-components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+const App = () => {
+  return (
+    <main>
+      <form>
+        <label htmlFor="">Usuario: </label>
+        <input type="text" placeholder="Usuario" />
+        <p>Lorem ipsum dolor sit amet.
+        </p>
+      </form>
+    </main>
+
+  );
+}
+
+export default App;
